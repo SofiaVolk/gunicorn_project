366e307803e6633cd27a997cae78f8e0255e25b1
shmakovofficial <xvsrvpxdfxeq94no97rhtdv3oivdwn@gmail.com>
2019-05-15 23:14:33 +0300

    Better performance

--- model/ml.py
+++ model/ml.py
@@ -8,7 +8,9 @@ import numpy as np
 from sklearn.ensemble import BaggingClassifier
 from sklearn.tree import DecisionTreeClassifier
 from pickle import dump, load
+from sklearn.neighbors import KNeighborsClassifier
 
+# раскомменить после слияния
 # from alembic.database1 import get_youla, get_vacancies
 
 CRITICAL_SIMILARITY = 0.75
@@ -18,6 +20,9 @@ with open(".data/categories_dict.pkl", "rb") as f:
 
 with open(".data/categories_model.pkl", "rb") as f:
     categories_model = load(f)
+
+with open(".data/dummy_model.pkl", "rb") as f:
+    dummy_model = load(f)
 
 w2v_model = KeyedVectors.load_word2vec_format(".data/araneum_upos_skipgram_300_2_2018.vec")
 
@@ -31,9 +36,6 @@ youla_annoy = AnnoyIndex(300)
 youla_annoy.load(".data/youla_annoy.ann")
 
 morph = MorphAnalyzer()
-
-with open(".data/dummy_model.pkl", "rb") as f:
-    dummy_model = load(f)
 
 
 class Model:
@@ -92,6 +94,11 @@ class Model:
             self.text2vec(text), number)
 
     def wish_detector(self, text):
+        """
+        Определение типа хотелки
+        :param text: текст хотелки
+        :return: тип хотелки
+        """
         vector = self.text2vec(text)
         decision = dummy_model.predict_proba([vector])[0]
         print(decision)
@@ -125,6 +132,7 @@ class Model:
         :return: category_id, category_name
         """
         data = pd.read_csv(".data/hh_dataset.csv").iloc[id]
+        # раскомментить после слияния
         # data = pd.DataFrame(get_vacancies(id=id, fields=["title", "description"]))
         return self.predict_text(data['title'] + ' ' + data['description'])
 
@@ -139,72 +147,102 @@ class Model:
         return category_id, categories_dict[category_id]
 
 
-class DataDumper:
+def dump_all():
     """
-    Сохранение данных с базы в локальные файлы для использования в Model
+    Дамп всего
     """
+    dump_hh()
+    dump_youla()
+    dump_categories()
+    dump_dummy()
 
-    def dump(self, hh=False, youla=False, with_categories=False):
-        """
-        Вызов методов
-        :param hh: сохранить данные с hh
-        :param youla: сохранить данные с юлы
-        :param with_categories: сохранить категории и переобучить предсказалку
-        :return: тапл названий сохраненных файлов
-        """
-        if hh:
-            self._dump_hh(with_categories=with_categories)
-        if youla:
-            self._dump_youla()
 
-    def _dump_hh(self, with_categories=False):
-        """
-        Сохранение данных с базы hh
-        :param with_categories: сохранить категории и переобучить предсказалку
-        """
-        data = pd.read_csv('.data/hh_dataset.csv')
-        # if with_categories:
-        #    data = pd.DataFrame(get_vacancies(fields=["title", "description"],
-        #                                      cats=["category_id", "category_name"]))
-        # else:
-        #    data = pd.DataFrame(get_vacancies(fields=["title", "description"]))
-        data.title.fillna('', inplace=True)
-        data.description.fillna(data.title, inplace=True)
-        data['text'] = data.title + ' ' + data.description
-        vec = np.empty((data.shape[0], 300), dtype=np.float)
-        for i in range(data.shape[0]):
-            vec[i] = Model.text2vec(data.iloc[i]['text'])
-        new_hh_annoy = AnnoyIndex(300)
-        for i, j in enumerate(vec):
-            new_hh_annoy.add_item(i, j)
-        new_hh_annoy.build(10)
-        new_hh_annoy.save(".data/new_hh_annoy.ann")
-        if with_categories:
-            new_categories_model = BaggingClassifier(DecisionTreeClassifier(min_samples_leaf=5))
-            new_categories_model.fit(vec, data['category_id'].values)
-            with open(".data/new_categories_model.pkl", "wb") as f:
-                dump(new_categories_model, f)
-            cats = data.drop_duplicates(subset='category_id')
-            new_categories_dict = dict()
-            for i in cats[['category_id', 'category_name']].values:
-                new_categories_dict[i[0]] = i[1]
-            with open(".data/new_categories_dict.pkl", "wb") as f:
-                dump(new_categories_dict, f)
+def dump_hh():
+    """
+    Сохранение данных с базы hh
+    """
+    data = pd.read_csv('.data/hh_dataset.csv')
+    # раскомментить после слияния веток
+    # data = pd.DataFrame(get_vacancies(fields=["title", "description"]))
+    data.title.fillna('', inplace=True)
+    data.description.fillna(data.title, inplace=True)
+    data['text'] = data.title + ' ' + data.description
+    vec = np.empty((data.shape[0], 300), dtype=np.float)
+    for i in range(data.shape[0]):
+        vec[i] = Model.text2vec(data.iloc[i]['text'])
+    new_hh_annoy = AnnoyIndex(300)
+    for i, j in enumerate(vec):
+        new_hh_annoy.add_item(i, j)
+    new_hh_annoy.build(10)
+    new_hh_annoy.save(".data/new_hh_annoy.ann")
 
-    def _dump_youla(self):
-        """
-        Сохранить данные с базы юлы
-        """
-        data = pd.read_csv('.data/youla_dataset.csv')
-        # data = pd.DataFrame(get_youla(fields=["title", "description"]))
-        data.title.fillna('', inplace=True)
-        data.description.fillna(data.title, inplace=True)
-        data['text'] = data.title + ' ' + data.description
-        vec = np.empty((data.shape[0], 300), dtype=np.float)
-        for i in range(data.shape[0]):
-            vec[i] = Model.text2vec(data.iloc[i]['text'])
-        new_youla_annoy = AnnoyIndex(300)
-        for i, j in enumerate(vec):
-            new_youla_annoy.add_item(i, j)
-        new_youla_annoy.build(10)
-        new_youla_annoy.save(".data/new_youla_annoy.ann")
+
+def dump_youla():
+    """
+    Сохранение данных с базы юлы
+    """
+    data = pd.read_csv('.data/youla_dataset.csv')
+    # раскомментить после слияния веток
+    # data = pd.DataFrame(get_youla(fields=["title", "description"]))
+    data.title.fillna('', inplace=True)
+    data.description.fillna(data.title, inplace=True)
+    data['text'] = data.title + ' ' + data.description
+    vec = np.empty((data.shape[0], 300), dtype=np.float)
+    for i in range(data.shape[0]):
+        vec[i] = Model.text2vec(data.iloc[i]['text'])
+    new_youla_annoy = AnnoyIndex(300)
+    for i, j in enumerate(vec):
+        new_youla_annoy.add_item(i, j)
+    new_youla_annoy.build(10)
+    new_youla_annoy.save(".data/new_youla_annoy.ann")
+
+
+def dump_categories():
+    """
+    Сохранение категорий и переобучение предсказалки
+    """
+    data = pd.read_csv('.data/hh_dataset.csv')
+    # раскомментить после слияния веток
+    # data = pd.DataFrame(get_vacancies(fields=["title", "description"],
+    #                                  cats=["category_id", "category_name"]))
+    cats = data.drop_duplicates(subset='category_id')
+    new_categories_dict = dict()
+    for i in cats[['category_id', 'category_name']].values:
+        new_categories_dict[i[0]] = i[1]
+    with open(".data/new_categories_dict.pkl", "wb") as f:
+        dump(new_categories_dict, f)
+    data = data.sample(10000)
+    data.title.fillna(data.description, inplace=True)
+    data['text'] = data.title
+    vec = np.empty((data.shape[0], 300), dtype=np.float)
+    for i in range(data.shape[0]):
+        vec[i] = Model.text2vec(data.iloc[i]['text'])
+    new_categories_model = BaggingClassifier(DecisionTreeClassifier(min_samples_leaf=5))
+    new_categories_model.fit(vec, data['category_id'].values)
+    with open(".data/new_categories_model.pkl", "wb") as f:
+        dump(new_categories_model, f)
+
+
+def dump_dummy():
+    """
+    Сохранение решающей модели
+    """
+    data_hh = pd.read_csv('.data/hh_dataset.csv').sample(10000)
+    data_youla = pd.read_csv('.data/youla_dataset.csv').sample(10000)
+    # раскомментить после слияния веток
+    # data_hh = pd.DataFrame(get_vacancies(fields=["title", "description"]))
+    # data_youla = pd.DataFrame(get_youla(fields=["title", "description"]))
+    for data in [data_hh, data_youla]:
+        data.title.fillna(data.description, inplace=True)
+        data['text'] = data.title
+    vec = np.empty((data_hh.shape[0] + data_youla.shape[0], 300), dtype=np.float)
+    ids = np.zeros(data_hh.shape[0] + data_youla.shape[0], dtype=np.int)
+    for i in range(data_hh.shape[0]):
+        vec[i] = Model.text2vec(data_hh.iloc[i]['text'])
+    for i in range(data_youla.shape[0]):
+        vec[i + data_hh.shape[0]] = Model.text2vec(data_youla.iloc[i]['text'])
+        ids[i + data_hh.shape[0]] = 1
+    new_dummy_model = KNeighborsClassifier()
+    new_dummy_model.fit(vec, ids)
+    with open(".data/new_dummy_model.pkl", "wb") as f:
+        dump(new_dummy_model, f)
