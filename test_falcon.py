6d9729bafac43e296e383e66c1163e7ed95973ae
Sofia Volkova <volkova300997@gmail.com>
2019-06-16 14:03:14 +0300

    change for version without conflicts

--- test_falcon.py
+++ test_falcon.py
@@ -1,10 +1,4 @@
 import falcon
-<<<<<<< HEAD
-from project_atom_app.logs import LOGGING
-from gunicorn.glogging import Logger
-
-#api_logger = Logger()
-=======
 import json
 import logging
 from project_atom_app.database1 import *
@@ -25,46 +19,11 @@ def form_response(result_list):
         result.append(result_dict)
         result_dict = {'title': None, 'description': None}
     return result
->>>>>>> gunicorn_ml
 
 
 class Resource(object):
     def on_post(self, req, resp):
         if req.content_length in (None, 0):
-<<<<<<< HEAD
-            #api_logger.info('Request failed <400>:\t' + 'no content')
-            return
-        body = req.stream.read()
-        print(body.decode('utf-8').split('"')[-2])
-        Logger.info('Request <200>')
-
-    '''
-    def load_config(self):
-        """Load configuration into Gunicorn."""
-        self.cfg.set('logger_class', ApiLogger)
-    '''
-
-app = falcon.API()
-resource = Resource()
-app.add_route('/', resource)
-
-
-'''
-from gunicorn.app.base import BaseApplication
-
-
-class Application(BaseApplication):
-    def load(self):
-        return app()
-if __name__ == '__main__':
-    options = {
-       'bind': '%s:%s' % ('127.0.0.1', '8080'),
-        'workers': 2,
-    }
-    #Application(options=settings.get('gunicorn', {})).run()
-    #Application(options=options).run()
-'''
-=======
             logger.info('Request failed <400>:\t' + 'no content')
             return
 
@@ -94,4 +53,3 @@ if __name__ == '__main__':
 app = falcon.API()
 resource = Resource()
 app.add_route('/', resource)
->>>>>>> gunicorn_ml
